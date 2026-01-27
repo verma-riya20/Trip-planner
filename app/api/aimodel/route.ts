1c457787b15c1d7e28334250c206922f128a3cd1
verma-riya20 <riyaverma3731@gmail.com>
2026-01-27 23:29:34 +0530

    open ai error resolved

--- app/api/aimodel/route.ts
+++ app/api/aimodel/route.ts
@@ -3,11 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import OpenAI from 'openai';
 import { aj } from "../arcjet/route";
-export const openai = new OpenAI({
-  baseURL: 'https://openrouter.ai/api/v1',
-  apiKey: process.env.OPENROUTER_API_KEY,
- 
-});
+import { openai } from "@/lib/openai";
 
 const PROMPT=`You are an AI Trip Planner Agent. Your goal is to help the user plan a trip by asking one relevant trip-related question at a time.
 Only ask questions about the following details in order, and wait for the user's answer before asking the next:
